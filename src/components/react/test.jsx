aef405a5c277b1456007efb94282a48f2847d8e1
root <root@DESKTOP-NQ5JUPK>
2023-06-06 22:37:56 -0700

    got summin

--- src/components/react/test.jsx
+++ src/components/react/test.jsx
@@ -1,28 +1,36 @@
-// import { useEffect, useState } from "react"
-// export default function Test () {
-//     useEffect(() => {
-//     const handleScroll = () => {
-//       console.log('User has scrolled');
-//       // Perform your actions based on scroll
-//     };
-    
-//     window.addEventListener('scroll', handleScroll);
 
-//     // Cleanup the event listener on component unmount
-//     return () => {
-//       window.removeEventListener('scroll', handleScroll);
-//     };
-//   }, [])
-//     return (
-//     <></>
-//     )
-// }
-
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Counter() {
   const [count, setCount] = useState(0);
+   useEffect(() => {
+    const handleScroll = (e) => {
+      console.log(e);
+      // Perform your actions based on scroll
+    };
+    
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("wheel", function(event) {
+  // Check scroll direction
+  if (event.deltaY > 0) {
+    // Scrolled down
+    console.log("Scrolled down");
+    // Perform your actions for scrolling down
+  } else {
+    // Scrolled up
+    console.log("Scrolled up");
+    // Perform your actions for scrolling up
+  }
+  });
 
+
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [])
+ 
   return (
     <div className="flex items-center justify-center gap-x-12">
       <button onClick={() => setCount((count) => count + 1)} className="px-4 py-2 text-white bg-indigo-600 rounded-md">
@@ -32,6 +40,7 @@ export default function Counter() {
       <button onClick={() => setCount((count) => count - 1)} className="px-4 py-2 text-white bg-red-600 rounded-md">
         Decrement
       </button>
+      
     </div>
   );
 }
